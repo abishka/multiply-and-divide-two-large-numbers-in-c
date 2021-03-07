0bf7cd2fadc5a7fd5105b6a00d49c970d6046e48
ata <ata.abishka@gmail.com>
2021-03-07 11:11:44 +0330

    ok multiplication

--- src/main.c
+++ src/main.c
@@ -1,19 +1,32 @@
+/**
+ * Author:    ata babaei   ata.abishka@gmail.com
+ * Created:   02.05.2021
+ * 
+ * https://github.com/abishka/multiply-and-divide-two-large-numbers-in-c
+ * 
+ * maktabkhoone c learneang prject
+ * master:  mr anvari 
+ **/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAXL 100
+
+#define MAXL 50
+
 //Function
 void multiplication();
+void clrscr();
 
 int main()
 {
     int ch;
 
-    while(1)
+    while (1)
     {
-        printf("\n*************** Multiplication ***************\n\n");
+        printf("\n\n\n*************** actions ***************\n\n");
 
         printf("1. Multiplication\n");
+        printf("2. Division\n");
         printf("0. Exit\n\n");
 
         puts("**********************************************");
@@ -21,11 +34,20 @@ int main()
         printf("Please choose an action: ");
         scanf(" %d", &ch);
 
-        switch(ch)
+        switch (ch)
         {
-            case 1: multiplication();
+        case 1:
+            multiplication();
+
             break;
-            case 0: exit(0); // Terminates the process
+
+        case 2:
+
+            break;
+
+        case 0:
+            return 0;
+            break;
         }
     }
 
@@ -34,118 +56,125 @@ int main()
 
 void multiplication()
 {
-    char n1[MAXL+1]; // Plus one is for null operator!
-    memset(n1, 0, MAXL*sizeof(char));
-    char n2[MAXL+1];
-    memset(n2, 0, MAXL*sizeof(char));
+    char n1[MAXL + 1]; // Plus one is for null operator!
+    memset(n1, 0, MAXL * sizeof(char));
+    char n2[MAXL + 1];
+    memset(n2, 0, MAXL * sizeof(char));
 
-    // use sample numbers or input numbers as string
-    char choice;
-    printf("Do you want to use sample numbers?\n"
-           "~~~> Yes: y/Y\n"
-           "~~~> No: n/N\n"
-           "Your choice:");
-    scanf(" %c", &choice);
+    printf("**************************    multiplication   ****************************\n");
 
-    if (choice == 'n' || choice =='N') // Enter numbers that you want
-    {
-        printf("Please enter the first number:\n");
-        scanf(" %s", n1);
-        printf("Please enter the second number:\n");
-        scanf(" %s", n2);
-    }
-    else if (choice == 'y' || choice == 'Y')
-    {
-        // Sample numbers
-        strcpy(n1, "11111111111111111111111111111111111111111111111122");
-        strcpy(n2, "00000000000000000000000000000000000000000000000012");
-
-        printf("\n-----------------Sample numbers-----------------\n");
-        printf("%s\n", n1);
-        printf("%s\n", n2);
-        printf("\n------------------------------------------------\n");
-    }
-    else
-    {
-        //return 0;
-		 printf("\n-------wrongggggggggggggg--------------\n");
-    }
-
-    // Put the longer number first
-    char temp[MAXL];
-    if (strlen(n1) < strlen(n2))
-    {
-        strcpy(temp, n1);
-        strcpy(n1, n2);
-        strcpy(n2, temp);
-    }
+    puts("Please enter the first number:");
+    scanf(" %s", n1);
+    puts("Please enter the second number:");
+    scanf(" %s", n2);
 
     // Length of numbers
     int L1 = strlen(n1);
     int L2 = strlen(n2);
 
-    int n=0, m;
-    int lent = 2*L1 ; // The resultant number length is at most 2 times the first number.
-    int carry=0, summ=0;
+    // Put the longer number first
+    char temp[MAXL + 1];
+    if (L1 < L2)
+    {
+        strcpy(temp, n1);
+        strcpy(n1, n2);
+        strcpy(n2, temp);
+        L1 = strlen(n1);
+        L2 = strlen(n2);
+    }
 
-    // steps of calculation, like using pen and paper
+    int lent = 2 * L1; // The resultant number length is at most 2 times the first number.
+    int carry = 0, summ = 0;
+
     int step[L2][lent];
-    memset(step, 0, L2*lent*sizeof(int));
+    memset(step, 0, L2 * lent * sizeof(int));
     // answer will be stored in result
     int result[lent];
-    memset(step, 0, lent*sizeof(int));
+    memset(result, 0, lent * sizeof(int));
+
+    clrscr();
+
+    // print first number
+    for (int i = 0; i < (lent - L1) + 2; i++)
+        printf(" ");
+    printf("%s\n", n1);
+
+    // print second number
+    printf("X ");
+    for (int i = 0; i < (lent - L2); i++)
+        printf(" ");
+    printf("%s\n", n2);
+
+    // print _____________ underline
+    for (int i = 0; i < (lent + 2); i++)
+        printf("_");
 
     // String of numbers to integer numbers
     int num1[L1];
     int num2[L2];
-    for (int i=0; i<L1; i++)
+    for (int i = 0; i < L1; i++)
     {
+        if (n1[i] < '0' || n1[i] > '9')
+        {
+            printf("\n\n\n ############ ERROR INPUT NUMBERS ###########");
+            exit(1);
+        }
+
         num1[i] = n1[i] - '0'; // Converts char to integer
     }
-    for (int i=0; i<L2; i++)
+    for (int i = 0; i < L2; i++)
     {
+        if (n2[i] < '0' || n1[i] > '9')
+        {
+            printf("\n\n\n ############ ERROR INPUT NUMBERS ###########");
+            exit(1);
+        }
         num2[i] = n2[i] - '0'; // Converts char to integer
     }
 
     // Multiplication by algorithm.
-    for (int i=L2-1; i>=0; i--)
+    int l2_lent = lent;
+    int n = 0, m;
+    for (int i = L2 - 1; i >= 0; i--)
     {
-        m = lent-1;
-        for (int j=L1-1; j>=0; j--)
+        m = l2_lent - 1;
+        for (int j = L1 - 1; j >= 0; j--)
         {
             step[n][m] = (carry + (num2[i] * num1[j])) % 10;
             carry = (carry + num2[i] * num1[j]) / 10;
-            m --;
+            m--;
 
-            if (m==lent-L1-1 && carry != 0) // L1 digits are passed. is there still carry? if yes insert it.
+            if (m == l2_lent - L1 - 1 && carry != 0) // L1 digits are passed. is there still carry? if yes insert it.
             {
                 step[n][m] = carry;
-                carry=0;
+                carry = 0;
             }
         }
-        n ++;
-        lent --;
+        n++;
+        l2_lent--;
     }
-    puts("");
+
+    puts("\n");
 
     // Print steps of calculations
     // from right, each digit of second number multiplied to first number
-    for (int i=0; i<L2; i++)
+    for (int i = 0; i < L2; i++)
     {
-        for (int j=0; j<2*L1; j++)
-        printf("%d", step[i][j]);
+        printf("  ");
+        for (int j = 0; j < lent; j++)
+            printf("%d", step[i][j]);
+
         puts(""); // Goes to the next line - i++
     }
-    puts("______________________________________________________________________");
 
     // Sum of steps
     carry = 0;
-    for (int i=2*L1-1; i>=0; i--) // F\Starting from right digit of each line
+    for (int i = lent - 1; i >= 0; i--) // F\Starting from right digit of each line
     {
-        for (int j=0; j<L2; j++) // Starting from first line
+        for (int j = 0; j < L2; j++) // Starting from first line
         {
             summ = summ + carry + step[j][i];
-            carry=0; // In each column Carry should be added to summ for once.
+            carry = 0; // In each column Carry should be added to summ for once.
         }
 
         if (summ >= 10)
@@ -157,28 +186,36 @@ void multiplication()
         else
         {
             carry = 0;
-            result[i] = summ ;
+            result[i] = summ;
         }
         summ = 0;
     }
 
-    // Print the result
-    printf("The answer is:\n");
-    int flag1=0;
-    for(int i=0; i<2*L1; i++)
+    //print + _________
+    puts("+");
+    for (int i = 0; i < (lent + 2); i++)
+        printf("_");
+
+    // **************************Print the result
+    printf("\n= ");
+    int flag1 = 0;
+    for (int i = 0; i < lent; i++)
     { //To remove redundant zeros at the beginning of the number
         if (result[i] == 0 && flag1 == 0)
-            continue;
+            printf(" ");
         else
         {
             printf("%d", result[i]);
-            flag1 =1; // Wherever the numbers began, this should capture them.
+            flag1 = 1; // Wherever the numbers began, this should capture them.
         }
-
     }
-    // if the result is zero or flag1=0
-    if (flag1 == 0)
-        printf("0");
+}
 
-   // return 0;
+void clrscr(void)
+{
+#if defined(_WIN32) || defined(_WIN64)
+    system("cls");
+#else
+    system("clear");
+#endif
 }
